873d389d3e4abfc4c664100e9deec29ebe8c36bf
Rahul Sheelavantar <rahulsheelavantar@gmail.com>
2022-10-07 14:16:04 +0530

    node day-2

--- node-day-2/index.js
+++ node-day-2/index.js
@@ -1,6 +1,22 @@
 const http = require("http");
 
+const PORT = 8080;
+
 const server = http.createServer((req, res) => {
   //Handler
+  res.write("Hello");
+  res.write(" ");
+  res.write("Rahul");
+  res.write(" ");
+  // res.end("!");
 
+  if (req.url === "/") return res.end("Welcome to homepage");
+  if (req.url === "/products") {
+    // res.setHeader("content-type", "application/json");
+    return res.end(JSON.stringify([1, 2, 3, 4]));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log("server started on localhost:" + PORT + "/");
 });
