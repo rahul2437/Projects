7c132d9d3a10cfb1dcefc6a52cb15b6807cd275c
Rahul Sheelavantar <rahulsheelavantar@gmail.com>
2023-01-12 23:00:40 +0530

    CRUD

--- Backend-CRUD/routes/books.route.js
+++ Backend-CRUD/routes/books.route.js
@@ -5,7 +5,7 @@ const { Book } = require("../models/book.model");
 const bookRouter = express.Router();
 
 bookRouter.get("/", async (req, res) => {
-  const { price_low, price_high, genre, title, author } = req.query;
+  const { price_low, price_high, genre, title, author, startsWith } = req.query;
   const filter = {};
   if (price_low && price_high) {
     filter.price = { $gte: +price_low, $lte: +price_high };
@@ -19,6 +19,9 @@ bookRouter.get("/", async (req, res) => {
   }
   if (author) {
     filter.author = { $regex: new RegExp(author, "i") };
+  }
+  if (startsWith) {
+    filter.title = { $regex: new RegExp("^" + startsWith, "i") };
   }
   let data = await Book.find(filter);
   res.send(data.length > 0 ? data : "No Book Found");
@@ -37,16 +40,20 @@ bookRouter.get("/:id", async (req, res) => {
 bookRouter.post("/addBook", validator, async (req, res) => {
   const payload = req.body;
   try {
-    const book = new Book(payload);
-    await book.save();
-    res.send("Book Added Successfully");
+    let data = await Book.find({ title: payload.title });
+    if (data) {
+      res.send("Book already exists");
+    } else {
+      const book = new Book(payload);
+      await book.save();
+      res.send("Book Added Successfully");
+    }
+    console.log("Book Found");
   } catch (error) {
     console.log({
       "err": "All the fields are not there"
     });
-    res.send({
-      "err": "All the fields are not there"
-    });
+    res.send(error.message);
   }
 });
 
